00fd76d53590a9bce540d70b26bf9b23807a77e1
Joe Nguwo <nguwojoe@gmail.com>
2026-03-05 14:48:02 +0200

    created customer middleware time checking incoming request

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import  dotenv  from "dotenv"
 import router from "./routes/getUsers.js"
 const app = express()
 
+// cutsome middieware
+function time(req,res,next){
+    console.log(`request day and time:  ${new Date().toDateString()} ${new Date().toLocaleTimeString()}`)
+    next()
+
+}
+app.use(time)
+app.use(express.json())
+
 dotenv.config()
 const PORT = process.env.PORT
 

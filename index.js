a10633ec293bc21494235b0ec17428f41fa2ad3c
Joe Nguwo <nguwojoe@gmail.com>
2026-03-05 16:17:46 +0200

    added the rate limit middleware

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-import express from "express"
-import  dotenv  from "dotenv"
-import router from "./routes/getUsers.js"
-const app = express()
+import express from "express";
+import rateLimit from "express-rate-limit";
+import dotenv from "dotenv";
+import router from "./routes/getUsers.js";
+const app = express();
 
 // cutsome middieware
-function time(req,res,next){
-    console.log(`request day and time:  ${new Date().toDateString()} ${new Date().toLocaleTimeString()}`)
-    next()
-
+function time(req, res, next) {
+  console.log(
+    `request day and time:  ${new Date().toDateString()} ${new Date().toLocaleTimeString()}`,
+  );
+  next();
 }
-app.use(time)
-app.use(express.json())
+const limiter = rateLimit({
+  max: 2,
+  windowMs: 60 * 1000, // one minute
+  handler: (req, res) => {
+    res.status(429).json({
+      status: 429,
+      message: "Too many requests from this IP. Please try again after 1 minute."
+    });
+  }
+});
+app.use(limiter)
+app.use(time);
+app.use(express.json());
 
-dotenv.config()
-const PORT = process.env.PORT
+dotenv.config();
+const PORT = process.env.PORT;
 
-app.use("/users",router)
+app.use("/users", router);
 
-
-app.listen(PORT,console.log(`server ruuning on port ${PORT}`))
+app.listen(PORT, console.log(`server ruuning on port ${PORT}`));
